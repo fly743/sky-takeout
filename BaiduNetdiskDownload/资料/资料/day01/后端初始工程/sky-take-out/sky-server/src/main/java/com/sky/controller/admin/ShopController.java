20350c14e012d9be77d6d3c34e8c76e606fb50fb
fuleyi <fly20050701@qq.com>
2026-04-04 11:36:39 +0800

    商品浏览

--- BaiduNetdiskDownload/资料/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ BaiduNetdiskDownload/资料/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -10,36 +10,26 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController("adminShopController")
 @RequestMapping("/admin/shop")
-@Api(tags = "店铺相关接口")
+@Api(tags="店铺相关接口")
 @Slf4j
 public class ShopController {
-
-    public static final String KEY = "SHOP_STATUS";
+    public static final String KEY="SHOP_STATUS";
     @Autowired
     private RedisTemplate redisTemplate;
-
-    /**
-     * 设置店铺营业状态
-     * @param status
-     * @return
-     */
+    //设置店铺营业状态
     @PutMapping("/{status}")
-    @ApiOperation("设置店铺营业状态")
-    public Result setStatus(@PathVariable Integer status){
-        log.info("设置店铺营业状态:{}",status == 1);
-        redisTemplate.opsForValue().set(KEY,status);
+    @ApiOperation("设置店铺的营业状态")
+    public Result setStatus( @PathVariable Integer status){
+        log.info("设置店铺的营业状态为：{}",status==1 ?"营业中":"打烊中");
+        redisTemplate.opsForValue().set("SHOP_STATUS",status);
         return Result.success();
     }
-
-    /**
-     * 获取店铺营业状态
-     * @return
-     */
-    @ApiOperation("获取店铺营业状态")
+    //获取店铺的营业状态
     @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
     public Result<Integer> getStatus(){
         Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
-        log.info("获取店铺营业状态:{}",status==1?"营业中":"打烊中");
+        log.info("获取到店铺的营业状态为：{}",status==1?"营业中":"打烊中");
         return Result.success(status);
     }
 }
