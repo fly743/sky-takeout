4f1f16022ddbc21699362e202128f95b277b626b
fuleyi <fly20050701@qq.com>
2026-04-06 12:55:42 +0800

    用户下单

---
+++ BaiduNetdiskDownload/资料/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.sky.service;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.Orders;
+import com.sky.vo.OrderSubmitVO;
+
+public interface OrderService {
+
+    OrderSubmitVO submitOrder(OrdersSubmitDTO orderSubmitDTO);
+
+}
