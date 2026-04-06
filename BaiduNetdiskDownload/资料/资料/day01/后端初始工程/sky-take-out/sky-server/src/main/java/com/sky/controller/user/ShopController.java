c4fec9e872f5eeffe454f330a8a64bffe38831c1
fuleyi <fly20050701@qq.com>
2026-04-06 14:51:08 +0800

    订单支付

--- BaiduNetdiskDownload/资料/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ BaiduNetdiskDownload/资料/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,4 +1,4 @@
-package com.sky.user;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
@@ -6,7 +6,9 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController("userShopController")
 @RequestMapping("/user/shop")
