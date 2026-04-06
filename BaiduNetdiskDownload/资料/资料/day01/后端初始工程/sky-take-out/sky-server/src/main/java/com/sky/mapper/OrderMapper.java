48ff852e96b8190ff5e243caf13a5a1150de4e1d
fuleyi <fly20050701@qq.com>
2026-04-06 15:26:25 +0800

    用户端历史订单模块

--- BaiduNetdiskDownload/资料/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ BaiduNetdiskDownload/资料/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -30,4 +32,17 @@ public interface OrderMapper {
     void update(Orders orders);
     @Update("update orders set status = #{orderStatus},pay_status = #{orderPaidStatus} ,checkout_time = #{check_out_time} where id = #{id}")
     void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time, Long id);
+    /**
+     * 订单分页查询
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+    /**
+     * 根据id查询订单
+     * @param id
+     * @return
+     */
+    @Select("select * from orders where id = #{id}")
+    Orders getById(Long id);
 }
